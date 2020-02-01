40c8c8c5dc2078e0cfb239c167189ac6575aa94d
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-01 23:13:32 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,16 +1,6 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {
-       :patriach => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => []
-     },
-     :capulet => {
-       :patriach => {},
-       :matriarch => {},
-       :heroine => {},
-       :heroine_friends => []
-     }
-   }
+  :montague => {},
+  :capulet => {}
+}
 end
